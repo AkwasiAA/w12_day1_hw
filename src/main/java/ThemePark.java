e1d20c0ba0b8cd8fc2988ca47fd571b8576014b8
Akwasi Atakora-Acheampong <aa.acheampong1@gmail.com>
2021-08-30 20:16:10 +0100

    MVP complete

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,12 @@
+import attractions.Attraction;
+import stalls.Stall;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+
+    private ArrayList<Attraction> attractions;
+    private ArrayList<Stall> stalls;
+
+
 }
