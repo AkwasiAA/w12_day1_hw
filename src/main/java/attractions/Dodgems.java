cbb97618a6323467d04060f8c39d9972b0eb9800
Akwasi Atakora-Acheampong <aa.acheampong1@gmail.com>
2021-08-30 17:26:44 +0100

    first commit

---
+++ src/main/java/attractions/Dodgems.java
@@ -0,0 +1,10 @@
+package attractions;
+
+import behaviours.ITicketed;
+
+public class Dodgems extends Attraction implements ITicketed {
+
+    public Dodgems(String name, int rating) {
+        super(name, rating);
+    }
+}
