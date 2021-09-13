46fee56f2026e47f6eaaa727018f1c13d129c66e
Akwasi Atakora-Acheampong <aa.acheampong1@gmail.com>
2021-09-13 20:20:18 +0100

    completed more tests

--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,21 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitor;
+    Visitor visitor1;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        visitor = new Visitor(14, 160, 25);
+        visitor1 = new Visitor(7, 90, 10);
     }
 
 
@@ -28,5 +33,20 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void hasPrice(){
+        assertEquals(4.50, dodgems.defaultPrice(),0.01);
+    }
+
+    @Test
+    public void willChargeVisitorOver12FullPrice(){
+        assertEquals(4.50, dodgems.priceFor(visitor), 0.01);
+    }
+
+    @Test
+    public void willChargeVisitorUnder12HalfPrice(){
+        assertEquals(2.25, dodgems.priceFor(visitor1), 0.01);
     }
 }
